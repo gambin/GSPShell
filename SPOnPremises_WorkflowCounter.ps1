d474f63174223c6257c23c9d8f5f399f9e464281
Tarcisio Gambin <gambin.br@gmail.com>
2017-10-11 17:24:01 -0300

    Update WF Counter file

--- SPOnPremises_WorkflowCounter.ps1
+++ SPOnPremises_WorkflowCounter.ps1
@@ -4,15 +4,15 @@ $sites = Get-SPSite -Limit All
 $total = 0
 foreach ($site in $sites){
     $webs = $site | Get-SPWeb -Limit All
-    foreach ($web in $webs){
-        foreach ($list in $web.lists){
-            foreach ($item in $list.workflowassociations | where {$_.runninginstances -gt 0}){
-                if ("$($web.Url)"){
-                    Write-Host "Site URL: $($web.Url)"
-                    Write-Host "- Lista: $($list.Title) ($($list.ItemCount))"
-                    Write-Host "- Lista URL: $($web.Url)/$($list.RootFolder.Url)"
-                    Write-Host "- Em Execucao: $($item.RunningInstances)"
-                    Write-Host
+	foreach ($web in $webs){
+		foreach ($list in $web.lists){
+			foreach ($item in $list.workflowassociations | where {$_.runninginstances -gt 0}){
+				if ("$($web.Url)"){
+					Write-Host "Site URL: $($web.Url)"
+					Write-Host "- List: $($list.Title) ($($list.ItemCount))"
+					Write-Host "- List URL: $($web.Url)/$($list.RootFolder.Url)"
+					Write-Host "- Running: $($item.RunningInstances)"
+					Write-Host
 					$total++
                 }
             }
@@ -20,4 +20,4 @@ foreach ($site in $sites){
     }
     $site.dispose()
 }
-Write-Host "Total de $($total) workflows em execução na farm!"
+Write-Host "$($total) workflows instances are running on this Farm!"
